56e9c5720fa5f07052d22720545aca330e04a5fb
Shopping <xpzheng6@163.com>
2022-12-25 19:12:29 +0800

    feat: update config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
+  base: '/',
+  appearance: true,
   title: 'Vitepress Template',
-  description: 'Vitepress documentation template project',
+  lastUpdated: true,
+  markdown: {
+    lineNumbers: true,
+  },
 
   themeConfig: {
+    lastUpdatedText: '最近更新于：',
+    outlineTitle: '目录',
     nav: [{
       text: '快速开始',
       link: '/start.md'

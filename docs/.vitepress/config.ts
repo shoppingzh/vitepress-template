14982070e373a2506d96639760437e5012332366
shoppingzh <xpzheng6@163.com>
2023-01-12 16:31:25 +0800

    feat: update config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,21 +1,32 @@
 import { defineConfig } from 'vitepress'
+import { autoGenerateSidebar } from 'press-util'
 
 export default defineConfig({
   base: '/',
   appearance: true,
   title: 'Vitepress Template',
   lastUpdated: true,
+  // 标签页logo
+  head: [
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }]
+  ],
   markdown: {
     lineNumbers: true,
   },
 
   themeConfig: {
-    lastUpdatedText: '最近更新于：',
+    logo: '/logo.svg',
+    lastUpdatedText: '最近更新于',
+    // 2/3/4级标题均形成目录
+    outline: [2, 4],
     outlineTitle: '目录',
     nav: [{
-      text: '快速开始',
+      text: '🎯 快速入手',
       link: '/start.md'
+    }, {
+      text: '🎨 使用技巧',
+      link: '/skills.md'
     }],
-
+    sidebar: autoGenerateSidebar as any,
   }
 })

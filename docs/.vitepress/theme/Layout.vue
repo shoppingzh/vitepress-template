872acecc1a2c72c329c108aae1384503cf122c3b
Shopping <xpzheng6@163.com>
2023-02-24 21:13:54 +0800

    feat: back top top

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <template #layout-top>
+      <el-backtop>
+        <svg-icon name="huojian" class="text-2xl" />
+      </el-backtop>
+    </template>
+  </Layout>
+</template>
+
+<script setup>
+// 具体使用参见：https://vitepress.vuejs.org/guide/theme-introduction#extending-the-default-theme
+import Theme from 'vitepress/theme'
+
+const { Layout } = Theme
+</script>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,9 +3,11 @@ import ui from '@/plugins/element-ui'
 import components from '@/components'
 import '@/styles/index.css'
 import 'virtual:svg-icons-register'
+import Layout from './Layout.vue'
 
 export default {
   ...Theme,
+  Layout,
   enhanceApp(ctx) {
     const { app } = ctx;
 
